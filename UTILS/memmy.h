7732e6d088f2027bcc44bf109f2f08ee446ff059
ereio <git@ere.io>
2015-09-13 00:48:16 -0400

    Adding memmy stuff to work with on desktop

--- UTILS/memmy.h
+++ UTILS/memmy.h
@@ -1,4 +1,20 @@
-#ifndef _SETUP
-#define _SETUP
+#ifndef _MEMMY
+#define _MEMMY
 
-int setup();
+int get_array_size(char *);
+
+int set_array_size(char **);
+
+char* set_memmy(int size);
+
+char** set_demmy(int rows, int cols);
+
+char* get_memmy(char* address);
+
+char** get_demmy(char** address);
+
+char* free_memmy(char* address);
+
+char** free_demmy(char** address);
+
+#endif
